a486c16891494a617a8cd0134a0b8b6c54c89df5
Rohil Gajera <rohilgajera@gmail.com>
2023-03-21 21:59:13 +0530

    message.

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use(errorController.getError);
+app.use(errorController.get404);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
--- controllers/error.js
+++ controllers/error.js
@@ -1,7 +1,3 @@
-exports.getError = (req, res, next) => {
-  // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-  res.status(404).render("404", { pageTitle: "Page Not Found", path: "/404" });
-
-  // For handlebars render pages and add css file -------------------------------------------------------------------
-  // .render("404", { pageTitle: "Page Not Found", errorCSS: true });
+exports.get404 = (req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
 };
